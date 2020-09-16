0b45427fe3dde3ee1ecabcc4191a89a5719cbc3c
mariobreu <mariobreu1005@gmail.com>
2020-09-16 16:25:31 +0200

    Sencond test

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World, changed!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
 
